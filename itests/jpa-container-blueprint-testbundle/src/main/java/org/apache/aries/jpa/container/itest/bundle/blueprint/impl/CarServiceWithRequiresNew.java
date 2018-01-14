d7dc64e2ede148ab8c6dac1637ee29171b41eb5c
Christian Schneider <cschneid@adobe.com>
2018-01-14 21:19:40 +0100

    User service inside test

--- itests/jpa-container-blueprint-testbundle/src/main/java/org/apache/aries/jpa/container/itest/bundle/blueprint/impl/CarServiceWithRequiresNew.java
+++ itests/jpa-container-blueprint-testbundle/src/main/java/org/apache/aries/jpa/container/itest/bundle/blueprint/impl/CarServiceWithRequiresNew.java
@@ -14,9 +14,14 @@ import javax.transaction.Transactional;
 import javax.transaction.Transactional.TxType;
 
 import org.apache.aries.jpa.container.itest.entities.Car;
+import org.apache.aries.jpa.container.itest.entities.CarService;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
 
 public class CarServiceWithRequiresNew extends AbstractCarServiceImpl
 {
+	BundleContext bundleContext;
 
     @Override
     @Transactional(TxType.REQUIRES_NEW)
@@ -37,11 +42,30 @@ public class CarServiceWithRequiresNew extends AbstractCarServiceImpl
     {
         Car c = new Car();
         c.setNumberPlate("TR123");
-        this.addCar(c);
-        return Arrays.asList(this.getCar("TR123"));
+        ServiceReference<CarService> ref = null;
+		try {
+        		ref = getService();
+			CarService carService = bundleContext.getService(ref);
+			carService.addCar(c);
+			return Arrays.asList(this.getCar("TR123"));
+		} finally {
+			if (ref != null) {
+				bundleContext.ungetService(ref);
+			}
+		}
     }
 
-    @Override
+	private ServiceReference<CarService> getService() {
+		try {
+			Collection<ServiceReference<CarService>> refs = bundleContext.getServiceReferences(CarService.class,
+					"(type=rn)");
+			return refs.iterator().next();
+		} catch (InvalidSyntaxException e) {
+			throw new RuntimeException(e.getMessage(), e);
+		}
+	}
+
+	@Override
     public void updateCar(Car car) {
     }
 
@@ -49,6 +73,10 @@ public class CarServiceWithRequiresNew extends AbstractCarServiceImpl
     public void deleteCar(String id) {
     }
 
+    public void setBundleContext(BundleContext bundleContext) {
+		this.bundleContext = bundleContext;
+	}
+    
 }
 
 
